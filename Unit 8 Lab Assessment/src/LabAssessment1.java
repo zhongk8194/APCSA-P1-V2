1426915666b3dd82427e2d6e967861ff88ec6c49
zhongk8194 <kellyzhong09@gmail.com>
2018-03-02 09:29:52 -0800

    Started Unit 10
    Started Unit 10

---
+++ Unit 8 Lab Assessment/src/LabAssessment1.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class LabAssessment1 {
+	
+	public static void main (String args[]) {
+		
+		Numbers one = new Numbers();
+		System.out.println(one.isGoofy(12));
+		System.out.println(Arrays.toString(one.getSomeGoofyNumbers(12)));
+	
+		}
+}
